4204e3be6bc90e50388a12997a48e0cbb3d5c08e
Pavel Feldman <pavel.feldman@gmail.com>
2022-05-07 14:37:49 -0700

    chore: split e2e and ct configs

---
+++ assets/playwright-ct.config.js
@@ -0,0 +1,52 @@
+// @ts-check
+const { devices } = require('{{ctPackageName}}');
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('{{ctPackageName}}').PlaywrightTestConfig}
+ */
+const config = {
+  testDir: './{{testDir}}',
+  /* Maximum time one test can run for. */
+  timeout: 10 * 1000,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+ 
+    /* Port to use for Playwright component endpoint. */
+    vitePort: 3100,
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+  ],
+};
+
+module.exports = config;
