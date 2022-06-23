6e436c619873aaa511261c059c3c7a91db0fcc36
Pavel Feldman <pavel.feldman@gmail.com>
2022-06-22 18:34:22 -0700

    chore: import Page via type

--- assets/example.spec.ts
+++ assets/example.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect, type Page } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
   await page.goto('https://demo.playwright.dev/todomvc');
