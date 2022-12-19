2ab2b84a5b4a189881be565973fb99f87867c7af
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2022-12-19 11:41:15 +0100

    chore: update examples (#71)

--- assets/example.spec.js
+++ assets/example.spec.js
@@ -1,21 +1,19 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('homepage has title and links to intro page', async ({ page }) => {
+test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
+});
 
-  // create a locator
-  const getStarted = page.getByRole('link', { name: 'Get started' });
-
-  // Expect an attribute "to be strictly equal" to the value.
-  await expect(getStarted).toHaveAttribute('href', '/docs/intro');
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await getStarted.click();
-  
+  await page.getByRole('link', { name: 'Get started' }).click();
+
   // Expects the URL to contain intro.
   await expect(page).toHaveURL(/.*intro/);
 });
