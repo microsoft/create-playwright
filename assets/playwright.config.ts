6f94c16b851eb2eb97e8ac6557bb79badd17cddb
Terry <zhangwenlp@vip.qq.com>
2023-05-31 17:31:13 +0800

    chore: update examples

--- assets/playwright.config.ts
+++ assets/playwright.config.ts
@@ -70,7 +70,7 @@ export default defineConfig({
     // },
     // {
     //   name: 'Google Chrome',
-    //   use: { ..devices['Desktop Chrome'], channel: 'chrome' },
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
 
