3dc1bf1f326f047dccb5b90f9769da725827fa09
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2024-07-06 14:19:06 +0200

    start with testing gitignore

--- tests/integration.spec.ts
+++ tests/integration.spec.ts
@@ -18,6 +18,8 @@ import path from 'path';
 import fs from 'fs';
 import childProcess from 'child_process';
 
+const validGitignore = 'node_modules/\n/test-results/\n/playwright-report/\n/blob-report/\n/playwright/.cache/\n';
+
 test('should generate a project in the current directory', async ({ run, dir, packageManager }) => {
   test.slow();
   const { stdout } = await run([], { installGitHubActions: true, testDir: 'tests', language: 'TypeScript', installPlaywrightDependencies: false, installPlaywrightBrowsers: true });
@@ -29,6 +31,7 @@ test('should generate a project in the current directory', async ({ run, dir, pa
   expect(playwrightConfigContent).toContain('tests');
   expect(fs.existsSync(path.join(dir, '.github/workflows/playwright.yml'))).toBeTruthy();
   expect(fs.existsSync(path.join(dir, '.gitignore'))).toBeTruthy();
+  expect(fs.readFileSync(path.join(dir, '.gitignore'), { encoding: 'utf8' })).toMatch(validGitignore);
   if (packageManager === 'npm') {
     expect(stdout).toContain('Initializing NPM project (npm init -y)…');
     expect(stdout).toContain('Installing Playwright Test (npm install --save-dev @playwright/test)…');
@@ -101,3 +104,11 @@ test('should generate in the root of pnpm workspace', async ({ run, packageManag
   expect(fs.existsSync(path.join(dir, 'package.json'))).toBeTruthy();
   expect(fs.existsSync(path.join(dir, 'playwright.config.ts'))).toBeTruthy();
 });
+
+test('should not duplicate gitignore entries', async ({ run, dir }) => {
+  await run([], { installGitHubActions: false, testDir: 'tests', language: 'TypeScript', installPlaywrightDependencies: false, installPlaywrightBrowsers: false });
+  expect(fs.readFileSync(path.join(dir, '.gitignore'), { encoding: 'utf8' })).toMatch(validGitignore);
+
+  await run([], { installGitHubActions: false, testDir: 'tests', language: 'TypeScript', installPlaywrightDependencies: false, installPlaywrightBrowsers: false });
+  expect(fs.readFileSync(path.join(dir, '.gitignore'), { encoding: 'utf8' })).toMatch(validGitignore);
+})
