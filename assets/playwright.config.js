cd7a1b24bccbae8fbd30d4276e1df01012da6ad3
Karl Horky <karl.horky@gmail.com>
2024-08-10 15:00:16 +0200

    Make ternary into single line

--- assets/playwright.config.js
+++ assets/playwright.config.js
@@ -21,9 +21,7 @@ module.exports = defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.GITHUB_ACTIONS
-    ? 'github'
-    : 'html',
+  reporter: process.env.GITHUB_ACTIONS ? 'github' : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
